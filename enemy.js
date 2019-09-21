71ba921c828d97580b6d7592ae579b4f7a19791d
samuel <samuechen@cs.stonybrook.edu>
2019-09-21 04:43:53 -0400

    spaz and charge

--- enemy.js
+++ enemy.js
@@ -1,10 +1,33 @@
 const variant = 
 {
-
-
-
+	//fast but unreliable
+	spaz : function(x, y, playerx, playery, difficulty)
+	{
+		let velocity = 2 * difficulty;
+		let threshold = (difficulty * .1) >= 1 ? .7 : (difficulty * .10); 
+		let xsign = Math.random() > threshold ? -1 : 1;
+		let ysign = Math.random() > threshold ? -1 : 1;
+		let xmove = xsign * (x < playerx ? velocity : -1 * velocity);
+		let ymove = ysign * (y < playery ? velocity : -1 * velocity);
+		return [xmove, ymove];
+	},
+	//slow but steady
+	charge : function(x, y, playerx, playery, difficulty)
+	{
+		let velocity = .5 * difficulty;
+		let xmove =  (x < playerx ? velocity : -1 * velocity);
+		let ymove =  (y < playery ? velocity : -1 * velocity);
+		return [xmove, ymove];
+	},
+	
+	//skilled
+	dodge: function()
+	{
+		//harry does this
+	}
 
 }
+
 
 
 class Enemy
@@ -16,9 +39,6 @@ class Enemy
 		this.color = color;
 		this.size = size;
 		this.variant = variant;
-		//this.x = positionx;
-		//this.y = positiony;
-
 		this.x = Math.floor(Math.random() * 500);
 		this.y = Math.floor(Math.random() * 500);
 	}
@@ -26,13 +46,10 @@ class Enemy
 	//return spaces it will be in 
 	makeMove(playerx, playery)
 	{
-		let xsign = Math.random() < .5 ? -1 : 1;
-		let ysign = Math.random() < .5 ? -1 : 1;
-		let xmove = xsign * Math.floor((Math.random() + 1) * this.difficulty);
-		let ymove = ysign * Math.floor((Math.random() + 1) * this.difficulty);
+		let move = variant[this.variant](this.x, this.y, playerx, playery, this.difficulty);
 		//return arguments to ctx.fillstyle
 		//posx, posy, sizex, sizey
-		return [this.x + xmove, this.y + ymove, this.size, this.size];
+		return [this.x + move[0], this.y + move[1], this.size, this.size];
 	}
 
 	//set coordinates
