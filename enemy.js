57a469c0017937ee83e29eae11189d076d131633
samuel <samuechen@cs.stonybrook.edu>
2019-09-21 01:33:16 -0400

    classes

---
+++ enemy.js
@@ -0,0 +1,17 @@
+class Enemy
+{ 
+	vx = 5;
+	vy = 5;
+	constructor(difficulty, color, variant)
+	{
+		this.difficulty = difficulty;
+		this.color = color;
+		this.variant;
+	}
+
+	move(playerx, playery,)
+	{
+
+	}
+
+}
